f55237f9b2b108eea324edbce5a4a248daa62291
nels.beckman <nels.beckman@71f115b6-0b72-11de-a6c4-5ba5c67cbe75>
2013-08-03 19:36:00 +0000

    Now we store the file name to disk as well.

--- Megabudget/src/com/nbeckman/megabudget/ChooseFileActivity.java
+++ Megabudget/src/com/nbeckman/megabudget/ChooseFileActivity.java
@@ -1,7 +1,11 @@
 package com.nbeckman.megabudget;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -10,6 +14,8 @@ import android.widget.ListView;
 // from that point forward.
 public class ChooseFileActivity extends Activity {
 
+	private String[] mSpreadsheetFileNames = new String[0];
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -19,14 +25,25 @@ public class ChooseFileActivity extends Activity {
 		// Get file names from input.
 		Bundle extras = getIntent().getExtras();
 		if (extras != null) {
-		    String[] file_names = extras.getStringArray("spreadsheet_files");
-		    System.err.println("file_names size is: " + file_names.length);
+			mSpreadsheetFileNames = extras.getStringArray("spreadsheet_files");
 		    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, 
-		            android.R.layout.simple_list_item_1, file_names);
+		            android.R.layout.simple_list_item_1, mSpreadsheetFileNames);
 			ListView list_view = 
 					(ListView)this.findViewById(R.id.file_chooser_list_view);
 			list_view.setAdapter(adapter);
+			list_view.setOnItemClickListener(mListClickedHandler); 
 		}	
 	}
 	
+	// Handler for when the user clicks on a file name.
+	private OnItemClickListener mListClickedHandler = new OnItemClickListener() {
+	    @Override
+	    public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+	    	// Return the file name to the calling activity.
+	    	Intent returnIntent = new Intent();
+	    	returnIntent.putExtra("result", mSpreadsheetFileNames[position]);
+	    	setResult(RESULT_OK, returnIntent);     
+	    	finish();
+	    }
+	};
 }
