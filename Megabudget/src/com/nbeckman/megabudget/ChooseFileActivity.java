616b678538963a33ffe9971262dd33981e6cf4ff
nels.beckman <nels.beckman@71f115b6-0b72-11de-a6c4-5ba5c67cbe75>
2013-10-05 14:26:28 +0000

    Yay! Now the choose file dialog box is working, so life is good.
    Next steps:
    - start up the app, check for an account & spreadsheet, then start loading the interface.

--- Megabudget/src/com/nbeckman/megabudget/ChooseFileActivity.java
+++ Megabudget/src/com/nbeckman/megabudget/ChooseFileActivity.java
@@ -3,39 +3,94 @@ package com.nbeckman.megabudget;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.google.gdata.data.spreadsheet.SpreadsheetEntry;
-
 import android.app.Activity;
 import android.app.LoaderManager;
 import android.content.AsyncTaskLoader;
 import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.android.gms.auth.GoogleAuthException;
+import com.google.gdata.data.spreadsheet.SpreadsheetEntry;
+import com.google.gdata.data.spreadsheet.SpreadsheetFeed;
+
 // This class shows users a list of available files, and then
 // forces them to choose the default file that will be edited
 // from that point forward.
+// TODO(nbeckman): Change name to FileManager?
 public class ChooseFileActivity extends Activity
 		implements LoaderManager.LoaderCallbacks<List<SpreadsheetEntry>> {
-
-	private String[] mSpreadsheetFileNames = new String[0];
 	
 	private final static int kSpreadsheetFileLoaderId = 1;
+	
+	// The preference name for the stored spreadsheet, picked initially and used forevermore
+	// after that.
+	public static final String kBudgetShreadsheetPreferencesName = "BUDGET_SPREADSHEET_PREFERENCES_NAME";
+	
+	// The preference name for the spreadsheet URL. Basically  pair with the above preference, except
+	// that one is for display, and this one is for actually changing/reading the spreadsheet.
+	public static final String kBudgetShreadsheetURLPreferencesName = "BUDGET_SPREADSHEET_URL_PREFERENCES_NAME";
+	
+	// Returns true if there is currently a set spreadsheet.
+	public static boolean hasStoredSpreadsheet(Context context) {
+		final SharedPreferences shared_pref = 
+				PreferenceManager.getDefaultSharedPreferences(context);
+		return shared_pref.contains(kBudgetShreadsheetPreferencesName);	
+	}
+	
+	// Gets the current spreadsheet in use as is stored in the shared preferences 
+	// manager. Because the spreadsheet may not have been set yet, this method
+	// can return an empty string.
+	public static String getStoredSpreadsheet(Context context) {
+		final SharedPreferences shared_pref = 
+				PreferenceManager.getDefaultSharedPreferences(context);
+		return shared_pref.getString(kBudgetShreadsheetPreferencesName, "");		
+	}
+
+	// Gets the current spreadsheet URL in use as is stored in the shared preferences 
+	// manager. Because the spreadsheet URL may not have been set yet, this method
+	// can return an empty string.
+	public static String getStoredSpreadsheetURL(Context context) {
+		final SharedPreferences shared_pref = 
+				PreferenceManager.getDefaultSharedPreferences(context);
+		return shared_pref.getString(kBudgetShreadsheetURLPreferencesName, "");		
+	}
+	
+	// Stores the given spreadsheet to the shared preferences store.
+	public static void storeSpreadsheet(Context context, String spreadsheet_name) {
+		final SharedPreferences shared_pref = 
+				PreferenceManager.getDefaultSharedPreferences(context);
+		Editor editor = shared_pref.edit();
+		editor.putString(kBudgetShreadsheetPreferencesName, spreadsheet_name);
+		editor.commit();
+	}
+	
+	// Stores the given spreadsheet URL to the shared preferences store.
+	public static void storeSpreadsheetURL(Context context, String spreadsheet_url) {
+		final SharedPreferences shared_pref = 
+				PreferenceManager.getDefaultSharedPreferences(context);
+		Editor editor = shared_pref.edit();
+		editor.putString(kBudgetShreadsheetURLPreferencesName, spreadsheet_url);
+		editor.commit();
+	}
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
 		// Show res/layout/file_chooser.xml
 		this.setContentView(R.layout.file_chooser);
 		
@@ -56,10 +111,22 @@ public class ChooseFileActivity extends Activity
 	private OnItemClickListener mListClickedHandler = new OnItemClickListener() {
 	    @Override
 	    public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-	    	// Return the file name to the calling activity.
 	    	Intent returnIntent = new Intent();
-	    	returnIntent.putExtra("result", mSpreadsheetFileNames[position]);
-	    	setResult(RESULT_OK, returnIntent);     
+	    	final Object item = parent.getItemAtPosition(position);
+	    	if (item instanceof SpreadsheetEntry) {
+	    		final SpreadsheetEntry spreadsheet = (SpreadsheetEntry)item;
+		    	// Return the file name to the calling activity.
+		    	// Store filename & worksheet feed URL
+	    		storeSpreadsheet(
+	    				ChooseFileActivity.this, spreadsheet.getTitle().getPlainText());
+	    		storeSpreadsheetURL(
+	    				ChooseFileActivity.this, spreadsheet.getWorksheetFeedUrl().toString());
+	    		System.err.println(spreadsheet.getWorksheetFeedUrl().toString());
+		    	returnIntent.putExtra("result", spreadsheet.getWorksheetFeedUrl().toString());
+		    	setResult(RESULT_OK, returnIntent);   
+	    	} else {
+	    		setResult(RESULT_CANCELED, returnIntent);   
+	    	}
 	    	finish();
 	    }
 	};
@@ -96,15 +163,40 @@ class SpreadsheetFilesLoader extends AsyncTaskLoader<List<SpreadsheetEntry>> {
 	@Override
 	public List<SpreadsheetEntry> loadInBackground() {
 		// Do the actual hard thing! Load the spreadsheets. 
-		// TODO Auto-generated method stub
-		return null;
+		if (!AccountManager.hasStoredAccount(getContext())) {
+			// TODO(nbeckman): What happens here if we don't have an account yet?
+			// Any good way to force one?
+			return new ArrayList<SpreadsheetEntry>(0);
+		}
+		final String account = AccountManager.getStoredAccount(getContext());
+		SpreadsheetFeed feed = null;
+		try { 
+			feed = SpreadsheetUtils.currentSpreadsheetFeedInThisThread(getContext(), account);
+		} catch (GoogleAuthException e) {
+			// TODO(nbeckman): Fast & loose here; can't we prevent this from happening?
+			e.printStackTrace();
+		}
+		if (feed == null) {
+			return new ArrayList<SpreadsheetEntry>(0);
+		}
+		return feed.getEntries();
+	}
+
+	@Override
+	protected void onStartLoading() {
+		// My understanding of this method is that it is called in
+		// response to startLoading(), which is called by the client
+		// probably after construction by the framework. forceLoad()
+		// then causes loadInBackground() to ultimately be called,
+		// delivering the results. I believe we should be caching these
+		// hard-earned spreadsheet files, but we won't until it becomes
+		// a problem. See these links for more:
+		// http://developer.android.com/reference/android/content/AsyncTaskLoader.html
+		// and
+		// http://stackoverflow.com/questions/17489145/populating-a-listview-using-asynctaskloader
+		forceLoad();
 	}
 	
-	// In examples, a number of other classes are overloaded, e.g.,
-	// http://developer.android.com/reference/android/content/AsyncTaskLoader.html
-	// and
-	// http://stackoverflow.com/questions/17489145/populating-a-listview-using-asynctaskloader
-	// but I will wait to see if they are needed.
 }
 
 // It basically knows how to take a spreadsheet and turn it into an indexable
